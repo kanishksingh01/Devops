2404ca15e0f9b8b943fc5506b133d344ad9e8e83
Kanishksingh01 <kanishk2607@gmail.com>
2025-06-06 16:29:29 -0400

    created a terraform folder and added basic EC2 script

---
+++ Terraform/EC2fast.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_instance" "web" {
+  ami           = "ami-0c55b159cbfafe1f0" # Amazon Linux 2
+  instance_type = "t2.micro"
+  tags = {
+    Name = "QuickStartInstance"
+  }
+}
